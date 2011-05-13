e04e71867a541ba060750a38d01c2e7f8c3cbbda
Ben Wong <ben.wong@unboxedconsulting.com>
2011-05-13 12:38:07 +0100

    switching to plain GAE project

---
+++ src/com/spannerinworks/storycloud/StorycloudServlet.java
@@ -0,0 +1,13 @@
+package com.spannerinworks.storycloud;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class StorycloudServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
